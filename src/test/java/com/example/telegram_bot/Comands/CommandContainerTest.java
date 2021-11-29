985682c9d1afc4a2d53663b38ffd56acb9286790
babyan <babyan1997@gmail.com>
2021-11-30 00:49:07 +0100

    JRBT-3 fixed a bug in the test

--- src/test/java/com/example/telegram_bot/Comands/CommandContainerTest.java
+++ src/test/java/com/example/telegram_bot/Comands/CommandContainerTest.java
@@ -38,6 +38,6 @@ class CommandContainerTest {
 
         Command command = commandContainer.retriveCommand(unknownCommand);
 
-        Assertions.assertNotEquals(UnknownCommand.class,command.getClass());
+        Assertions.assertEquals (UnknownCommand.class,command.getClass());
     }
 }
