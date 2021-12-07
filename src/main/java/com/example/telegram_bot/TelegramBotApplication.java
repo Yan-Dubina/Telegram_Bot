ab51decc0d383fcb71af474514845e1758b202fd
babyan <babyan1997@gmail.com>
2021-12-07 18:32:45 +0100

    JRBT-6 Added Command AddGroup.

--- src/main/java/com/example/telegram_bot/TelegramBotApplication.java
+++ src/main/java/com/example/telegram_bot/TelegramBotApplication.java
@@ -1,12 +1,16 @@
 package com.example.telegram_bot;
 
+import jdk.nashorn.internal.runtime.logging.DebugLogger;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.logging.LoggingApplicationListener;
 
 @SpringBootApplication
 public class TelegramBotApplication {
 
+
     public static void main(String[] args) {
+        System.out.println(LoggingApplicationListener.CONFIG_PROPERTY);
         SpringApplication.run(TelegramBotApplication.class, args);
     }
 
