9de1b683c7deb2609cc6396997c2726aea24e398
babyan <babyan1997@gmail.com>
2021-12-07 19:32:05 +0100

    JRBT-6 Added Command AddGroup.

--- src/test/java/com/example/telegram_bot/Services/GroupSubServiceTest.java
+++ src/test/java/com/example/telegram_bot/Services/GroupSubServiceTest.java
@@ -1,35 +1,42 @@
 package com.example.telegram_bot.Services;
-
+import com.example.telegram_bot.Client.TelegramBotGroupClient;
 import com.example.telegram_bot.dto.GroupDiscussionInfo;
 import com.example.telegram_bot.repository.GroupSubRepository;
 import com.example.telegram_bot.repository.entity.GroupSub;
 import com.example.telegram_bot.repository.entity.TelegramUser;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
 
 import java.util.Optional;
 
-import static org.junit.jupiter.api.Assertions.*;
+@DisplayName("Unit-level testing for GroupSubService")
+public class GroupSubServiceTest {
 
-class GroupSubServiceTest {
     private GroupSubService groupSubService;
     private GroupSubRepository groupSubRepository;
+    private TelegramBotGroupClient telegramBotGroupClient;
     private TelegramUser newUser;
 
-    private final static String CHAT_ID = "1";
+    private final static Long CHAT_ID = 1234234L;
+    private final static Integer GROUP_ID = 1123;
+    private final static Integer LAST_POST_ID = 310;
 
     @BeforeEach
     public void init() {
         TelegramUserService telegramUserService = Mockito.mock(TelegramUserService.class);
         groupSubRepository = Mockito.mock(GroupSubRepository.class);
+        telegramBotGroupClient = Mockito.mock(TelegramBotGroupClient.class);
         groupSubService = new GroupSubServiceImpl(groupSubRepository, telegramUserService);
 
         newUser = new TelegramUser();
         newUser.setActive(true);
-        newUser.setChatId(CHAT_ID);
+        newUser.setChatId(CHAT_ID.toString());
 
-        Mockito.when(telegramUserService.findByChatId(CHAT_ID)).thenReturn(Optional.of(newUser));
+        Mockito.when(telegramUserService.findByChatId(CHAT_ID.toString())).thenReturn(Optional.of(newUser));
+
+        //Mockito.when(telegramBotGroupClient.findLastPostId(GROUP_ID)).thenReturn(LAST_POST_ID);
     }
 
     @Test
@@ -37,16 +44,17 @@ class GroupSubServiceTest {
         //given
 
         GroupDiscussionInfo groupDiscussionInfo = new GroupDiscussionInfo();
-        groupDiscussionInfo.setId(1);
+        groupDiscussionInfo.setId(GROUP_ID);
         groupDiscussionInfo.setTitle("g1");
 
         GroupSub expectedGroupSub = new GroupSub();
         expectedGroupSub.setId(groupDiscussionInfo.getId());
         expectedGroupSub.setTitle(groupDiscussionInfo.getTitle());
+
         expectedGroupSub.addUser(newUser);
 
         //when
-        groupSubService.save(CHAT_ID, groupDiscussionInfo);
+        groupSubService.save(CHAT_ID.toString(), groupDiscussionInfo);
 
         //then
         Mockito.verify(groupSubRepository).save(expectedGroupSub);
@@ -56,7 +64,7 @@ class GroupSubServiceTest {
     public void shouldProperlyAddUserToExistingGroup() {
         //given
         TelegramUser oldTelegramUser = new TelegramUser();
-        oldTelegramUser.setChatId("2");
+        oldTelegramUser.setChatId(String.valueOf(2L));
         oldTelegramUser.setActive(true);
 
         GroupDiscussionInfo groupDiscussionInfo = new GroupDiscussionInfo();
@@ -77,12 +85,11 @@ class GroupSubServiceTest {
         expectedGroupSub.addUser(newUser);
 
         //when
-        groupSubService.save(CHAT_ID, groupDiscussionInfo);
+        groupSubService.save(CHAT_ID.toString(), groupDiscussionInfo);
 
         //then
         Mockito.verify(groupSubRepository).findById(groupDiscussionInfo.getId());
         Mockito.verify(groupSubRepository).save(expectedGroupSub);
     }
-
 
 }
